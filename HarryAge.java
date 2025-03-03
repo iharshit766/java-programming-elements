edd8b490cc3eb26f6fd7809dfac705c557700b9f
iharshit766 <harshi766766@gmail.com>
2025-03-03 13:56:47 +0530

    [Harshit] ADD: Calculating Harry's Age

--- HarryAge.java
+++ HarryAge.java
@@ -1,8 +1,16 @@
 public class HarryAge {
     public static void main(String[] args) {
-        int birthYear = 2000;
-        int currentYear = 2024;
-        int age = currentYear - birthYear;
-        System.out.println("Harry's age in 2024 is " + age);
+        // store birth year
+        int birthYear = 2000;  
+        
+        // store current year
+        int currentYear = 2024;  
+        
+        // calculate age
+        int age = currentYear - birthYear;  
+        
+        // print the result
+        System.out.println("Harry's age in 2024 is " + age);  
     }
 }
+
